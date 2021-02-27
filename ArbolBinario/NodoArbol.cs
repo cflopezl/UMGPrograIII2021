e2b105fd4367d1418107ee9b1f0dffb42ccde15d
cflopezl <cflopezl@gmail.com>
2021-02-27 16:02:05 -0600

    Ejemplo visto en clase de arbol binario

---
+++ ArbolBinario/NodoArbol.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArbolBinario
+{
+    class NodoArbol<T>
+    {
+        public T data { get; set; }
+        public NodoArbol<T> izq { get; set; }
+        public NodoArbol<T> der { get; set; }
+    }
+}
---
+++ ArbolBinario/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ArbolBinario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NodoArbol<string> jesus = new NodoArbol<string>
+            { data = "M Jesus",
+                der = new NodoArbol<string> { data = "Abel", der = null, izq = null },
+                izq = new NodoArbol<string> { data = "Anyora", der = null, izq = null },
+            };
+            NodoArbol<string> esperanza = new NodoArbol<string>
+            {
+                data = "Esperanza",
+                der = new NodoArbol<string> { data = "Rodrigo", der = null, izq = null },
+                izq = new NodoArbol<string> { data = "Maria", der = null, izq = null },
+            };
+            NodoArbol<string> raiz = new NodoArbol<string>
+            { data = "Esperanza", der = jesus, izq = esperanza };
+        }
+    }
+}
