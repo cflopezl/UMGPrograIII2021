e978a4221fdcc02722023ce06e14e20cc7dcc743
cflopezl <cflopezl@gmail.com>
2021-03-06 15:38:28 -0600

    Ejemplo Arbol binario de búsqueda

---
+++ ArbolBinario/ArbolBinarioBusqueda.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArbolBinario
+{
+    class ArbolBinarioBusqueda<T>
+    {
+        public NodoArbol<T> raiz { get; set; }
+        
+        public ArbolBinarioBusqueda()
+        {
+            raiz = null;
+        }
+
+        public void Insertar(T valor,
+            Func<T, T, bool> MenorQue, Func<T, T, bool> MayorQue)
+        {
+            raiz = Insertar(raiz, valor, MenorQue, MayorQue);
+        }
+
+        public NodoArbol<T> Insertar(NodoArbol<T> raizSub, T valor, 
+            Func<T, T, bool>MenorQue, Func<T, T, bool> MayorQue)
+        {
+            if (raizSub == null)
+            {
+                raizSub = new NodoArbol<T> 
+                    { data = valor, izq = null, der = null };
+            }else if( MenorQue(valor, raizSub.data) )
+            {
+                raizSub.izq = Insertar(raizSub.izq, valor, MenorQue, MayorQue);
+            }
+            else if ( MayorQue(valor, raizSub.data))
+            {
+                raizSub.der = Insertar(raizSub.der, valor, MenorQue, MayorQue);
+            }
+            else throw new Exception("Nodo duplicado");
+
+            return raizSub;
+        }
+
+        public void MostrarArbol(NodoArbol<T> raiz, String espacio)
+        {
+            if (raiz == null)
+            {
+                Console.WriteLine(espacio + "null");
+            }
+            else
+            {
+                MostrarArbol(raiz.der, espacio + "         ");
+                Console.WriteLine(espacio + "      /");
+                Console.WriteLine(espacio + raiz.data);
+                Console.WriteLine(espacio + "      \\");
+                MostrarArbol(raiz.izq, espacio + "         ");
+            }
+        }
+
+    }
+}
--- ArbolBinario/Program.cs
+++ ArbolBinario/Program.cs
@@ -6,7 +6,7 @@ namespace ArbolBinario
     {
         static void Main(string[] args)
         {
-            NodoArbol<string> jesus = new NodoArbol<string>
+            /*NodoArbol<string> jesus = new NodoArbol<string>
             { data = "M Jesus",
                 der = new NodoArbol<string> { data = "Abel", der = null, izq = null },
                 izq = new NodoArbol<string> { data = "Anyora", der = null, izq = null },
@@ -18,7 +18,20 @@ namespace ArbolBinario
                 izq = new NodoArbol<string> { data = "Maria", der = null, izq = null },
             };
             NodoArbol<string> raiz = new NodoArbol<string>
-            { data = "Esperanza", der = jesus, izq = esperanza };
+            { data = "Esperanza", der = jesus, izq = esperanza };*/
+
+            ArbolBinarioBusqueda<int> abbenteros = new ArbolBinarioBusqueda<int>();
+            //delegado mayor y menor para enteros
+            Func<int, int, bool> MenorQueEntero = (x, y) => x < y;
+            Func<int, int, bool> MayorQueEntero = (x, y) => x > y;
+            //agregar nuevas llaves a la EDD
+            abbenteros.Insertar(50, MenorQueEntero, MayorQueEntero);
+            abbenteros.Insertar(35, MenorQueEntero, MayorQueEntero);
+            abbenteros.Insertar(80, MenorQueEntero, MayorQueEntero);
+            abbenteros.Insertar(10, MenorQueEntero, MayorQueEntero);
+
+            abbenteros.MostrarArbol(abbenteros.raiz,"");
+
         }
     }
 }
